a742f8a48ccfaedd038bc5f0647f62de89a6087e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-13 11:31:49 -0500

    Feat: add all methods for venue, tests pass

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,30 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    @patrons.map do |patron|
+      patron.upcase
+    end
+  end
+
+  def over_capacity?
+    @patrons.length > 3
+  end
+
+  def kick_out
+    until over_capacity? == false
+      @patrons.pop
+    end
+  end
 end
